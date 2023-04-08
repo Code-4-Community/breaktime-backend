7a4846d9a8b489e6c5c9f5fffa433dee1a31e574
izzyconner <32255130+izzyconner@users.noreply.github.com>
2023-04-08 10:14:07 -0400

    start of group authentication

--- src/aws/auth.service.ts
+++ src/aws/auth.service.ts
@@ -11,6 +11,11 @@ export class AuthService {
   async verifyJwt(jwt: string): Promise<Boolean> {
     try {
       const userPayload = await this.cognitoService.validate(jwt);
+      console.log(userPayload);
+      // TODO grab cognito groups from the user Payload, may want a secondary parent function that
+      // both authenticates the jwt and any user/group specifications
+      const groups = userPayload['cognito:groups'];
+      console.log(groups);
     
       return true; 
     } catch (e) {

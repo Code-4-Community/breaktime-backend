f7b368d87441594e2b413a513d55a8a578e6c1a9
izzyconner <conner.i@husky.neu.edu>
2023-04-16 15:50:12 -0400

    Finishing documentation, some testing

--- src/aws/guards/roles.guard.ts
+++ src/aws/guards/roles.guard.ts
@@ -1,24 +1,39 @@
 import { Injectable, CanActivate, ExecutionContext } from '@nestjs/common';
 import { Reflector } from '@nestjs/core';
+import { AuthVerificationResponse } from '../auth.service';
 
 
-// TODO : currently allows all roles to access
+/**
+ * Nest guard for role-based access. Reference: https://docs.nestjs.com/guards
+ */
 @Injectable()
 export class RolesGuard implements CanActivate {
     constructor(private reflector: Reflector) {}
 
+    /**
+     * Determines if a given request can be executed based on the role's of the requesting user.
+     * @param context the execution context passed from the previous step in the pipeline. 
+     * Should contain the request and user payload from AuthService. For more information, see https://docs.nestjs.com/fundamentals/execution-context
+     * @returns if the endpoint can be run for the given user.
+     */
     canActivate(context: ExecutionContext): boolean {
-      const roles = this.reflector.get<string[]>('roles', context.getHandler());
-      /*if (!roles) {
-        return true;
-      }*/
+      // Get the roles defined for the endpoint that are allowed to access it.
+      const allowedRoles = this.reflector.get<string[]>('roles', context.getHandler());
 
-      const request = context.switchToHttp().getRequest();
-      const user = request.user;
-      console.log(user);
-      if (user.groups.length != 0) {
+      // If there are no specified roles for the endpoint, allow any user to access.
+      if (!allowedRoles) {
         return true;
       }
-      return false; // TODO : will need to match roles
+
+      // Get the request from the last-run step of the pipeline (in this case, the AuthMiddleware) and pull the necessary user data
+      // that has been set. Keep in mind, this is not necessarily the actual user payload or JWT from the original request. Instead,
+      // this is what has been set by the previous step(s).
+      const request = context.switchToHttp().getRequest();
+      const user: AuthVerificationResponse = request.user;
+
+      // Check that the user belongs to at least one of the required roles
+      const validUserGroups = user.groups.filter(group => allowedRoles.includes(group));
+      console.log(validUserGroups);
+      return validUserGroups.length > 0;
     }
 }

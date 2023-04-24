ea6aff28f55d4d0dcd4e4db876afa64ad50e5908
izzyconner <32255130+izzyconner@users.noreply.github.com>
2023-04-24 19:55:48 -0400

    Adding more comments

--- src/aws/decorators/roles.decorators.ts
+++ src/aws/decorators/roles.decorators.ts
@@ -1,3 +1,6 @@
 import { SetMetadata } from '@nestjs/common';
 
+/**
+ * A custom decorator that allows us to take in a set of groups or roles and process them in the Nest pipeline (i.e. RolesGuard).
+ */
 export const Roles = (...roles: string[]) => SetMetadata('roles', roles);

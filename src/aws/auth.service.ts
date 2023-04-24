7910285226b6f38cb13ad89c0ee42e496e03498c
izzyconner <32255130+izzyconner@users.noreply.github.com>
2023-04-24 19:54:50 -0400

    Small cleanup, more comments

--- src/aws/auth.service.ts
+++ src/aws/auth.service.ts
@@ -8,11 +8,17 @@ export class AuthService {
     private cognitoService: CognitoService,
   ) {}
 
+  /**
+   * 
+   * @param jwt The access token to be validated
+   * @returns if the validation was successful and what Cognito groups the user is a part of
+   */
   async verifyJwt(jwt: string): Promise<AuthVerificationResponse> {
     try {
       const userPayload = await this.cognitoService.validate(jwt);
       
       // grab cognito groups from the user Payload to reutrn in the verification response.
+      // This is so that the role-based access is determined by Cognito groups
       const groups = userPayload['cognito:groups'];
     
       return {isValidated: true, groups: groups}; 
--- src/aws/guards/roles.guard.ts
+++ src/aws/guards/roles.guard.ts
@@ -4,14 +4,14 @@ import { AuthVerificationResponse } from '../auth.service';
 
 
 /**
- * Nest guard for role-based access. Reference: https://docs.nestjs.com/guards
+ * Nest guard for role-based access (i.e. groups from Cognito). Reference: https://docs.nestjs.com/guards
  */
 @Injectable()
 export class RolesGuard implements CanActivate {
     constructor(private reflector: Reflector) {}
 
     /**
-     * Determines if a given request can be executed based on the role's of the requesting user.
+     * Determines if a given request can be executed based on the roles of the requesting user.
      * @param context the execution context passed from the previous step in the pipeline. 
      * Should contain the request and user payload from AuthService. For more information, see https://docs.nestjs.com/fundamentals/execution-context
      * @returns if the endpoint can be run for the given user.
@@ -28,11 +28,14 @@ export class RolesGuard implements CanActivate {
       // Get the request from the last-run step of the pipeline (in this case, the AuthMiddleware) and pull the necessary user data
       // that has been set. Keep in mind, this is not necessarily the actual user payload or JWT from the original request. Instead,
       // this is what has been set by the previous step(s).
+      // In our case, this is from the Cognito groups a user is assigned.
       const request = context.switchToHttp().getRequest();
       const user: AuthVerificationResponse = request.user;
 
       // Check that the user belongs to at least one of the required roles
       const validUserGroups = user.groups.filter(group => allowedRoles.includes(group));
+
+      // For testing purposes, print out the groups a user is a part of that are allowed to access this resource
       console.log(validUserGroups);
       return validUserGroups.length > 0;
     }
